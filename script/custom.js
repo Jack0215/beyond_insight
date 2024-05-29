c1d9d6d6719adb7414c6fde21362d5677cf24689
Jack Lee <jaeyoon-lee@naver.com>
2024-05-29 16:14:58 +0900

    mobile version add

--- script/custom.js
+++ script/custom.js
@@ -2,12 +2,29 @@ $(function () {
   /* Include Files */
   $(".header-include").load("/include/header.html", function () {
     /*Mega Navigaion */
-    $(".trigger").click(function () {
-      $(".mega-navi").stop().slideToggle();
-    });
-    $("section").click(function () {
-      $(".mega-navi").stop().slideUp();
-    });
+    $(window)
+      .resize(function () {
+        /* 디바이스 너비가 767px 이상일 때(태블릿, PC 때) */
+        if ($(window).innerWidth() > 767) {
+          $(".trigger").click(function () {
+            $(".mega-navi").stop().slideToggle();
+          });
+          $("section").click(function () {
+            $(".mega-navi").stop().slideUp();
+          });
+        } else {
+          $(".trigger").click(function () {
+            $(".mega-navi").animate({ left: 0 }, 300);
+          });
+          $("section").click(function () {
+            $(".mega-navi").animate({ left: -300 }, 300);
+          });
+          $(".btn-mega-navi-close").click(function () {
+            $(".mega-navi").animate({ left: -300 }, 300);
+          });
+        }
+      })
+      .resize();
 
     /* Member Register */
     $(".toggle-pw").click(function () {
@@ -33,16 +50,31 @@ $(function () {
       $(".member-login-overlay").fadeOut();
       $("body").removeClass("active");
     });
+
+    /* Mega Navi */
+    $(".mega-navi-item b").click(function () {
+      // $(this).next().stop().slideToggle(200);
+
+      $(".mega-navi-item-wrap").slideUp(200);
+      $(this).next().stop().slideDown(200);
+      $(this).addClass("active");
+      $(this).parent().siblings().children("b").removeClass("active");
+    });
   });
 
-  $(".footer-include").load("/include/footer.html");
-  /* Scroll Header Fixed*/
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-      $("header").addClass("active");
-    } else {
-      $("header").removeClass("active");
-    }
+  $(".footer-include").load("/include/footer.html", function () {
+    /*Footer LNB */
+    $(".link-item-title").click(function () {
+      $(this).next().stop().slideToggle(300);
+      $(this).toggleClass("active");
+    });
+
+    /* company-info-trigger */
+    $(".company-info-trigger").click(function () {
+      // $("address").toggle();
+      $(this).next().stop().slideToggle(300);
+      $(this).toggleClass("active");
+    });
   });
 
   /*The Final CountDown */
@@ -100,8 +132,18 @@ $(function () {
   });
 
   /* Class Detail Navigation */
-  $(".class-detail-navigation a").click(function () {
+  $(".class-detail-navigation a").click(function (e) {
     $(this).addClass("active").siblings().removeClass("active");
+
+    /* Smooth Scrolling */
+    var linklocation = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(linklocation).offset().top,
+      },
+      500
+    );
+    e.preventDefault();
   });
 
   /* Class Curriculum */
@@ -110,22 +152,73 @@ $(function () {
     $(this).toggleClass("active");
   });
 
-  /* Front Slider */
-  $(".class-share.front-slider-items").slick({
-    infinity: true,
-    arrows: false,
-    dots: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    fade: false,
-    speed: 500,
-    pauseOnHover: true,
+  /* Class faq */
+
+  $(".faq-title").click(function () {
+    // 클린한 것 펼치고 나머지는 접기
+    // $(this).siblings(".faq-content").stop().slideUp(300);
+    // $(this).next().stop().slideDown(200);
+
+    $(this).next().slideToggle();
   });
-  /* Fouce Class Slider */
+  /* Front Slider */
+  $(".front-slider-items").slick({
+    infinite: true, // 마지막 슬라이드 다음에 처음으로 돌아가기
+    arrows: false, // 다음 이전 버튼
+    dots: false, // 동그라미 도트 버튼
+    autoplay: true, // 자동재생
+    autoplaySpeed: 3000, // 슬라이드가 머무는 시간
+    fade: false, // 크로스페이드로 할지 여부
+    speed: 300, // 슬라이드 교차하는 시간
+    pauseOnHover: true, // 마우스 올라갔을 때 잠시 멈춤
+    responsive: [
+      {
+        breakpoint: 1024, //breakpoint : 너비를 얘기함
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+          dots: false,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  /* Focus Class Slider */
   $(".focus-class-items").slick({
-    slidesToShow: 4, //최초에 보이는 갯수
-    slidesToScroll: 2, //슬라이드 몇개씩 할 건지 갯수
+    slidesToShow: 4, // 최초에 보이는 개수
+    slidesToScroll: 2, // 슬라이드할 때 몇개씩 슬라이드할지 개수
     arrows: true,
     dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          dots: false,
+          arrows: false,
+        },
+      },
+    ],
   });
 });
